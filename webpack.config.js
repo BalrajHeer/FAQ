c1612bc20ba1473b3e1bc57e0796f654f069d879
Vivek <vxsangwan@gmail.com>
2025-01-28 16:07:12 -0500

    commit4

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development", // Change to "production" for production build
-  entry: "./faq.js", // Entry point (JS file)
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"), // Output folder
-  },
-  devServer: {
-    static: "./dist",
-    hot: true,
-    port: 9000, // Change if needed
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/, // SCSS to CSS
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.html$/, // Handle HTML files
-        use: ["html-loader"],
-      },
-      {
-        test: /\.js$/, // JS processing
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader", // Optional if using ES6+ features
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
+    entry: {
+        main: './src/index.js',
+        faq: './src/faq.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './faq.html',
+            chunks: ['faq'],
+            filename: 'faq.html',
+        }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./index.html", // Use index.html as the template
-      filename: "index.html",
-    }),
-    new MiniCssExtractPlugin({ filename: "styles.css" }),
-  ],
+    devServer: {
+        static: './dist',
+        port: 9000,
+    },
+    mode: 'development', // Ensure to set the mode to 'development' or 'production' as per your needs
 };
